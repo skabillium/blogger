f647316f8207c52daedd8558529da71dcb4d64ce
skabi <skabillium@gmail.com>
2019-04-24 19:00:06 +0300

    added user delete route

--- server/routes/users.js
+++ server/routes/users.js
@@ -77,4 +77,15 @@ router.post('/login', async (req, res, next) => {
     });
 });
 
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then(result => {
+            res.status(200).json({ message: 'User deleted' })
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
 export default router
