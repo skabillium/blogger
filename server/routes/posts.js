ab0a598dab7fd6a4b93fbe614ce599e3bff15099
skabi <skabillium@gmail.com>
2019-04-25 19:56:36 +0300

    initial commit for client app

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,11 +7,11 @@ import * as postController from '../controllers/posts'
 
 const router = express.Router();
 
-router.post('/', checkAuth, validateUser,postController.add_post);
+router.post('/', checkAuth, validateUser, postController.add_post);
 
 router.get('/:user_id', checkAuth, postController.get_by_user);
 
-router.patch('/:post_id', checkAuth, validateUser,postController.update_post);
+router.patch('/:post_id', checkAuth, validateUser, postController.update_post);
 
 router.delete('/:post_id', checkAuth, validateUser, postController.delete_post);
 
