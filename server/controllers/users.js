69a99c3fe8335519d86fee0da7717a661d1fc167
skabi <skabillium@gmail.com>
2019-05-01 16:37:21 +0300

    created client app project layout

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,85 +1,97 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-import User from '../models/User'
+import User from "../models/User";
 
 export const signup = async (req, res, next) => {
+  const users = await User.find({ email: req.body.email }).exec();
 
-    const users = await User.find({ email: req.body.email }).exec();
-
-    if (users.length >= 1) {
-        return res.status(409).json({ message: 'Mail exists' });
-    } else {
-        
-        const user = await User.find({ username: req.body.username }).exec();
-
-        if (user.length >= 1) {
-            return res.status(409).json({ message: 'Username taken' });
-        } else {
-
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json(err);
-                } else {
-                    const newUser = new User({
-                        _id: mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        username: req.body.username,
-                        password: hash
-                    });
-
-                    newUser.save()
-                        .then(result => {
-                            res.status(201).json({ message: 'User created', user: result });
-                        })
-                        .catch(err => {
-                            res.status(500).json(err);
-                        });
-                }
-            });
-        }
-    }
-}
-
-export const login = async (req, res, next) => {
-
+  if (users.length >= 1) {
+    return res.status(409).json({ message: "Mail exists" });
+  } else {
     const user = await User.find({ username: req.body.username }).exec();
 
-    if (user.length < 1) {
-        return res.status(401).json({
-            message: 'Auth failed1'
-        });
+    if (user.length >= 1) {
+      return res.status(409).json({ message: "Username taken" });
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json(err);
+        } else {
+          const newUser = new User({
+            _id: mongoose.Types.ObjectId(),
+            email: req.body.email,
+            username: req.body.username,
+            password: hash
+          });
+
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            process.env.JWT_KEY,
+            { expiresIn: "1d" }
+          );
+
+          newUser
+            .save()
+            .then(result => {
+              res
+                .status(201)
+                .json({ message: "User created", user: result, token });
+            })
+            .catch(err => {
+              res.status(500).json(err);
+            });
+        }
+      });
+    }
+  }
+};
+
+export const login = async (req, res, next) => {
+  const user = await User.find({ username: req.body.username }).exec();
+
+  if (user.length < 1) {
+    return res.status(401).json({
+      message: "Auth failed1"
+    });
+  }
+
+  bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+    if (err) {
+      return res.status(401).json({
+        message: "Auth failed2"
+      });
     }
 
-    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-            return res.status(401).json({
-                message: 'Auth failed2'
-            });
-        }
+    if (result) {
+      const token = jwt.sign(
+        {
+          email: user[0].email,
+          userId: user[0]._id
+        },
+        process.env.JWT_KEY,
+        { expiresIn: "1d" }
+      );
 
-        if (result) {
-            const token = jwt.sign({
-                email: user[0].email,
-                userId: user[0]._id
-            }, process.env.JWT_KEY, { expiresIn: '1d' });
-
-            return res.status(200).json({
-                message: 'Auth succesful',
-                token
-            });
-        }
-    });
-}
+      return res.status(200).json({
+        message: "Auth succesful",
+        token
+      });
+    }
+  });
+};
 
 export const _delete = (req, res, next) => {
-    User.remove({ _id: req.params.userId })
-        .exec()
-        .then(result => {
-            res.status(200).json({ message: 'User deleted' })
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
-}
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then(result => {
+      res.status(200).json({ message: "User deleted" });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 // Create the user model for mongoose
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: {
-        type: String,
-        required: true,
-        createIndexes: true
-    },
-    password: { type: String, required: true },
-    username: { type: String, required: true }
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    createIndexes: true
+  },
+  password: { type: String, required: true },
+  username: { type: String, required: true }
 });
 
-export default mongoose.model('User', userSchema)
+export default mongoose.model("User", userSchema);
