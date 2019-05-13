e60a19e1dd7650e1a7b4473c44eaa0d046bfd8a7
skabi <skabillium@gmail.com>
2019-05-13 19:53:11 +0300

    added clear database script for development

--- server/app.js
+++ server/app.js
@@ -1,55 +1,45 @@
-import express from 'express'
-import morgan from 'morgan'
-import mongoose from 'mongoose'
+import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import userRoutes from './routes/users'
-import postRoutes from './routes/posts'
+import userRoutes from "./routes/users";
+import postRoutes from "./routes/posts";
 
 const app = express();
 
 // Connect to database
-mongoose.connect(process.env.DB_URL,{
-    useNewUrlParser: true
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true
 });
 mongoose.Promise = global.Promise;
 
 // Use morgan and body parser middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Grant ability to send requests to browsers outside of localhost
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+app.use(cors());
 
-    if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE. GET');
-        return res.status(200).json({});
-    }
-    next();
-});
-
-app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // If request doesn't get handled by the routers above
 // throw error
 app.use((req, res, next) => {
-
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
-export default app
+export default app;
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,57 +7,60 @@ import User from "../models/User";
 export const signup = async (req, res, next) => {
   try {
     const users = await User.find({ email: req.body.email }).exec();
+
+    if (users.length >= 1) {
+      return res.status(409).json({ message: "Mail exists" });
+    } else {
+      const user = await User.find({ username: req.body.username }).exec();
+
+      if (user.length >= 1) {
+        return res.status(409).json({ message: "Username taken" });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json(err);
+          } else {
+            const newUser = new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              username: req.body.username,
+              password: hash
+            });
+
+            const token = jwt.sign(
+              {
+                email: newUser.email,
+                userId: newUser._id
+              },
+              process.env.JWT_KEY,
+              { expiresIn: "1d" }
+            );
+
+            newUser
+              .save()
+              .then(result => {
+                res
+                  .status(201)
+                  .json({ message: "User created", user: result, token });
+              })
+              .catch(err => {
+                res.status(500).json(err);
+              });
+          }
+        });
+      }
+    }
   } catch (error) {
     return res.status(500).json({ message: "db error" });
-  }
-
-  if (users.length >= 1) {
-    return res.status(409).json({ message: "Mail exists" });
-  } else {
-    const user = await User.find({ username: req.body.username }).exec();
-
-    if (user.length >= 1) {
-      return res.status(409).json({ message: "Username taken" });
-    } else {
-      bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-          return res.status(500).json(err);
-        } else {
-          const newUser = new User({
-            _id: mongoose.Types.ObjectId(),
-            email: req.body.email,
-            username: req.body.username,
-            password: hash
-          });
-
-          const token = jwt.sign(
-            {
-              email: user[0].email,
-              userId: user[0]._id
-            },
-            process.env.JWT_KEY,
-            { expiresIn: "1d" }
-          );
-
-          newUser
-            .save()
-            .then(result => {
-              res
-                .status(201)
-                .json({ message: "User created", user: result, token });
-            })
-            .catch(err => {
-              res.status(500).json(err);
-            });
-        }
-      });
-    }
   }
 };
 
 export const login = async (req, res, next) => {
-  const user = await User.find({ username: req.body.username }).exec();
-
+  try {
+    const user = await User.find({ username: req.body.username }).exec();
+  } catch (error) {
+    return res.status(500).json({ message: "db error" });
+  }
   if (user.length < 1) {
     return res.status(401).json({
       message: "Auth failed1"
