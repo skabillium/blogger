86da36d91ab13faf91125773457ad6b9d4f7912c
skabi <skabillium@gmail.com>
2019-06-04 03:19:18 +0300

    added get post by id route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,63 +1,67 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-import Post from '../models/Post'
+import Post from "../models/Post";
 
 export const get_by_user = async (req, res, next) => {
-
-    try {
-        const posts = await Post.find({ user_id: req.params.user_id }).exec();
-        if (posts.length > 0) {
-            res.status(200).json(posts);
-        } else {
-            res.status(204).json({ message: 'No available posts for user' });
-        }
-    } catch (err) {
-        res.status(500).json(err);
+  try {
+    const posts = await Post.find({ user_id: req.params.user_id }).exec();
+    if (posts.length > 0) {
+      res.status(200).json(posts);
+    } else {
+      res.status(204).json({ message: "No available posts for user" });
     }
-}
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 export const add_post = (req, res, next) => {
-    
-    const newPost = new Post({
-        _id: mongoose.Types.ObjectId(),
-        posted: new Date,
-        user_id: req.body.user_id,
-        title: req.body.title,
-        content: req.body.content
-    });
+  const newPost = new Post({
+    _id: mongoose.Types.ObjectId(),
+    posted: new Date(),
+    user_id: req.body.user_id,
+    title: req.body.title,
+    content: req.body.content
+  });
 
-    newPost.save()
-        .then(result => {
-            res.status(201).json({ message: 'Post created', post: result });
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
-}
+  newPost
+    .save()
+    .then(result => {
+      res.status(201).json({ message: "Post created", post: result });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
+
+export const get_post = (req, res, next) => {
+  Post.findById(req.params.post_id)
+    .exec()
+    .then(post => res.status(200).json({ post }))
+    .catch(err => res.status(500).json(err));
+};
 
 export const update_post = (req, res, next) => {
+  const id = req.params.post_id;
+  let updateOpts = req.body;
 
-    const id = req.params.post_id;
-    let updateOpts = req.body;
-
-    Post.updateOne({_id: id}, { $set: updateOpts})
-        .exec()
-        .then(result => {
-            res.status(200).json({ message: 'Post updated' });
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
-}
+  Post.updateOne({ _id: id }, { $set: updateOpts })
+    .exec()
+    .then(result => {
+      res.status(200).json({ message: "Post updated" });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
 
 export const delete_post = (req, res, next) => {
-
-    Post.remove({ _id: req.params.post_id })
-        .exec()
-        .then(result => {
-            res.status(200).json({ message: 'Post deleted' });
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
-}
+  Post.remove({ _id: req.params.post_id })
+    .exec()
+    .then(result => {
+      res.status(200).json({ message: "Post deleted" });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,6 +9,8 @@ const router = express.Router();
 
 router.post("/", checkAuth, validateUser, postController.add_post);
 
+router.get("/:post_id", checkAuth, postController.get_post);
+
 router.get("/get_by_user/:user_id", checkAuth, postController.get_by_user);
 
 router.patch("/:post_id", checkAuth, validateUser, postController.update_post);
